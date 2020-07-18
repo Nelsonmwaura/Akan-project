e8904592be5d177849927de530af6e7e5b09fb0a
Nelsonmwaura <Nelsontoby5@gmail.com>
2020-07-18 12:41:04 +0300

    Adding bootstrap file

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
 function SUBMIT() {
-  var date = parseInt(document.getElementById("Day").value);
-  var month = parseInt(document.getElementById("Month").value) - 1;
-  var year = parseInt(document.getElementById("Year").value);
+  var date = document.getElementById('date').value;
+  var month = document.getElementById('month').value;
+  var yearOfBirth = document.getElementById('year').value;
 
-  if (day < 0 || day > 31) {
-    document.getElementsByClassName("invalid day").innerHtml = "Enter valid Date";
-    return false;
-  } else if (date === "") {
-    document.getElementsByClassName("invalid day").innerHtml = "insert field";
-    return false;
-  } else {
-    date.setDate(date);
-  }
-  if (month < 0 || day > 12) {
-    document.getElementsByClassName("invalid month").innerHtml = "Enter a valid Month";
-    return false;
-  } else if (month === "") {
-    document.getElementsByClassName("invalid month").innerHtml = "insert field";
-    return false;
-  } else {
-    date.setMonth(month);
-  }
-  if (year < 0) {
-    document.getElementsByClassName("invalid year").innerHtml = "Enter valid Year";
-    return false;
+  if (date === "" || date < 1 || date > 31) {
+    alert("Date must be between 1 and 31");
+  } else if (month === "" || month < 1 || month > 12) {
+    alert("Date must be between 1 and 12");
   } else if (year === "") {
-    document.getElementsByClassName("inalid year").innerHtml = "insert field";
-    return false;
+    alert("Enter year");
   } else {
-    date.setYear(year);
+    return false;
   }
+}
+function getAkan() {
+  var date = document.getElementById('date').value;
+  var month = document.getElementById('month').value;
+  var yearOfBirth = document.getElementById('year').value;
+  var century = yearOfBirth.slice(0, 2);
+  var year = yearOfBirth.slice(2, 4);
 
   var genders = document.getElementById("gender");
   var gender = genders.options[genders.selectedIndex].text;
@@ -38,46 +27,41 @@ function SUBMIT() {
   var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var female = ["Akosua", "Adwoa", " Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-  var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  var dayOfBirth = (((century / 4) - 2 * year - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
   var dayOfBirth = Math.floor(dayOfBirth);
 
-  function getAkan() {
-    var ismale = document.getElementById('male').checked;
-    var isfemale = document.getElementById('female').checked;
-    if (ismale === true && isfemale === false)
-      if (day === 0) {
-        alert("your name is" + male[0]);
-      }
-    else if (day === 1) {
-      alert("your name is" + male[1]);
-    } else if (day === 2) {
-      alert("your name is" + male[2]);
-    } else if (day === 3) {
-      alert("your name is" + male[3]);
-    } else if (day === 4) {
-      alert("your name is" + male[4]);
-    } else if (day === 5) {
-      alert("your name is" + male[5]);
-    } else if (day === 6) {
-      alert("your name is" + male[6]);
-    }
-  }
-  if (isfemale === true && ismale === false) {
+  if (ismale === true && isfemale === false)
     if (day === 0) {
-      alert("your name is" + female[0]);
+      alert("your name is" + male[0]);
     } else if (day === 1) {
-      alert("your name is" + female[1]);
-    } else if (day === 2) {
-      alert("your name is" + female[2]);
-    } else if (day === 3) {
-      alert("your name is" + female[3]);
-    } else if (day === 4) {
-      alert("your name is" + female[4]);
-    } else if (day === 5) {
-      alert("your name is" + female[5]);
-    } else if (day === 6) {
-      alert("your name is" + Female[6]);
-    }
+    alert("your name is" + male[1]);
+  } else if (day === 2) {
+    alert("your name is" + male[2]);
+  } else if (day === 3) {
+    alert("your name is" + male[3]);
+  } else if (day === 4) {
+    alert("your name is" + male[4]);
+  } else if (day === 5) {
+    alert("your name is" + male[5]);
+  } else if (day === 6) {
+    alert("your name is" + male[6]);
   }
+} else if (isfemale === true && ismale === false) {
+  if (day === 0) {
+    alert("your name is" + female[0]);
+  } else if (day === 1) {
+    alert("your name is" + female[1]);
+  } else if (day === 2) {
+    alert("your name is" + female[2]);
+  } else if (day === 3) {
+    alert("your name is" + female[3]);
+  } else if (day === 4) {
+    alert("your name is" + female[4]);
+  } else if (day === 5) {
+    alert("your name is" + female[5]);
+  } else if (day === 6) {
+    alert("your name is" + Female[6]);
+  }
+}
 }
 }
