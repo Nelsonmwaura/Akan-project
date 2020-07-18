57c943872e9aaa7127254f15061ef7bd990ad9fc
Nelsonmwaura <Nelsontoby5@gmail.com>
2020-07-18 11:47:41 +0300

    Adding  formula on js file

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function SUBMIT() {
-  var d = new Date();
   var date = parseInt(document.getElementById("Day").value);
   var month = parseInt(document.getElementById("Month").value) - 1;
   var year = parseInt(document.getElementById("Year").value);
@@ -7,70 +6,73 @@ function SUBMIT() {
   if (day < 0 || day > 31) {
     document.getElementsByClassName("invalid day").innerHtml = "Enter valid Date";
     return false;
-  } else if (date == "") {
+  } else if (date === "") {
     document.getElementsByClassName("invalid day").innerHtml = "insert field";
     return false;
   } else {
-    d.setDate(date);
+    date.setDate(date);
   }
   if (month < 0 || day > 12) {
     document.getElementsByClassName("invalid month").innerHtml = "Enter a valid Month";
     return false;
-  } else if (month == "") {
+  } else if (month === "") {
     document.getElementsByClassName("invalid month").innerHtml = "insert field";
     return false;
   } else {
-    d.setMonth(month);
+    date.setMonth(month);
   }
   if (year < 0) {
     document.getElementsByClassName("invalid year").innerHtml = "Enter valid Year";
     return false;
-  } else if (year == "") {
+  } else if (year === "") {
     document.getElementsByClassName("inalid year").innerHtml = "insert field";
     return false;
   } else {
-    d.setYear(year);
+    date.setYear(year);
   }
   var day = d.getDay();
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var female = ["Akosua", "Adwoa", " Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+  var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  var dayOfBirth = Math.floor(dayOfBirth);
+
   function getAkan() {
     var ismale = document.getElementById('male').checked;
     var isfemale = document.getElementById('female').checked;
-    if (ismale == true && isfemale == false)
-      if (day == 0) {
+    if (ismale === true && isfemale === false)
+      if (day === 0) {
         alert("your name is" + male[0]);
       }
-    else if (day == 1) {
+    else if (day === 1) {
       alert("your name is" + male[1]);
-    } else if (day == 2) {
+    } else if (day === 2) {
       alert("your name is" + male[2]);
-    } else if (day == 3) {
+    } else if (day === 3) {
       alert("your name is" + male[3]);
-    } else if (day == 4) {
+    } else if (day === 4) {
       alert("your name is" + male[4]);
-    } else if (day == 5) {
+    } else if (day === 5) {
       alert("your name is" + male[5]);
-    } else if (day == 6) {
+    } else if (day === 6) {
       alert("your name is" + male[6]);
     }
   }
-  if (isfemale == true && ismale == false) {
-    if (day == 0) {
+  if (isfemale === true && ismale === false) {
+    if (day === 0) {
       alert("your name is" + female[0]);
-    } else if (day == 1) {
+    } else if (day === 1) {
       alert("your name is" + female[1]);
-    } else if (day == 2) {
+    } else if (day === 2) {
       alert("your name is" + female[2]);
-    } else if (day == 3) {
+    } else if (day === 3) {
       alert("your name is" + female[3]);
-    } else if (day == 4) {
+    } else if (day === 4) {
       alert("your name is" + female[4]);
-    } else if (day == 5) {
+    } else if (day === 5) {
       alert("your name is" + female[5]);
-    } else if (day == 6) {
+    } else if (day === 6) {
       alert("your name is" + Female[6]);
     }
   }
