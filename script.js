c9ba7dc30cd8d49a5967e3e138fdaa6a8493bbfa
Nelsonmwaura <Nelsontoby5@gmail.com>
2020-07-20 10:22:07 +0300

    Adding css styles

--- script.js
+++ script.js
@@ -1,67 +1,48 @@
-function SUBMIT() {
+function SUBMIT(){
   var date = document.getElementById('date').value;
   var month = document.getElementById('month').value;
   var yearOfBirth = document.getElementById('year').value;
 
-  if (date === "" || date < 1 || date > 31) {
+  if(date===""||date<1||date>31){
     alert("Date must be between 1 and 31");
-  } else if (month === "" || month < 1 || month > 12) {
+  }
+  else if(month===""||month<1||month>12){
     alert("Date must be between 1 and 12");
-  } else if (year === "") {
-    alert("Enter year");
-  } else {
+  }
+  else if(year===""){
+    alert("You must enter a value year");
+  }
+  else{
     return false;
   }
 }
-function getAkan() {
+
+function getAkanName(){
   var date = document.getElementById('date').value;
+
   var month = document.getElementById('month').value;
+
   var yearOfBirth = document.getElementById('year').value;
-  var century = yearOfBirth.slice(0, 2);
-  var year = yearOfBirth.slice(2, 4);
+
+
+  var century = yearOfBirth.slice(0,2);
+  var year = yearOfBirth.slice(2,4);
 
   var genders = document.getElementById("gender");
   var gender = genders.options[genders.selectedIndex].text;
 
-  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var female = ["Akosua", "Adwoa", " Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
 
-  var dayOfBirth = (((century / 4) - 2 * year - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
+  var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
   var dayOfBirth = Math.floor(dayOfBirth);
 
-  if (ismale === true && isfemale === false)
-    if (day === 0) {
-      alert("your name is" + male[0]);
-    } else if (day === 1) {
-    alert("your name is" + male[1]);
-  } else if (day === 2) {
-    alert("your name is" + male[2]);
-  } else if (day === 3) {
-    alert("your name is" + male[3]);
-  } else if (day === 4) {
-    alert("your name is" + male[4]);
-  } else if (day === 5) {
-    alert("your name is" + male[5]);
-  } else if (day === 6) {
-    alert("your name is" + male[6]);
-  }
-} else if (isfemale === true && ismale === false) {
-  if (day === 0) {
-    alert("your name is" + female[0]);
-  } else if (day === 1) {
-    alert("your name is" + female[1]);
-  } else if (day === 2) {
-    alert("your name is" + female[2]);
-  } else if (day === 3) {
-    alert("your name is" + female[3]);
-  } else if (day === 4) {
-    alert("your name is" + female[4]);
-  } else if (day === 5) {
-    alert("your name is" + female[5]);
-  } else if (day === 6) {
-    alert("your name is" + Female[6]);
-  }
-}
-}
+  if (gender === "Male") {
+       document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  maleName[dayOfBirth -1];
+       }
+       else if (gender === "Female") {
+           document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  femaleName[dayOfBirth -1];
+       }
+
 }
